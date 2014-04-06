123154ecf88ee248953fe441405f8d2ecaa2fe99
Alexandr Smaga <asmaga@magecore.com>
2014-04-06 23:43:06 +0000

    refs #51    - removed test code

--- app/code/local/TC/ProductSegmentation/Helper/Data.php
+++ app/code/local/TC/ProductSegmentation/Helper/Data.php
@@ -7,17 +7,4 @@
  */
 class TC_ProductSegmentation_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function test()
-    {
-        $this->setChild(
-            'reset_filter_button',
-            $this->getLayout()->createBlock('adminhtml/widget_button')
-                ->setData(
-                    array(
-                         'label'   => Mage::helper('adminhtml')->__('Reset Filter'),
-                         'onclick' => $this->getJsObjectName() . '.resetFilter()',
-                    )
-                )
-        );
-    }
 }
